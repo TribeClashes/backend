f18e449a36d39b5c7920bd873ce8cb72f1422e9e
Plummy <plummybeatsoff@gmail.com>
2024-12-23 20:00:03 +0100

    Made redis necessary

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ from pydantic_settings import BaseSettings
 class Config(BaseSettings):
     api_key: SecretStr
     database_dsn: SecretStr | None = None
-    redis_dsn: SecretStr | None = None
+    redis_dsn: SecretStr
